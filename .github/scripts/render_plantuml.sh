563f803498f093d2205ba40649b3b743d4de0cfb
dlandi <dennis@dennislandi.com>
2025-06-15 14:10:44 -0400

    Update render_plantuml.sh

--- .github/scripts/render_plantuml.sh
+++ .github/scripts/render_plantuml.sh
@@ -33,25 +33,29 @@ find "$INDIR" -type f -name "*.md" | while read -r mdfile; do
         i=$((i+1))
       done
       i=$((i+1))
-      pumlfile="tmp_plantuml_extract_${block_num}.puml"
-      printf "%s\n" "${block_lines[@]}" > "$pumlfile"
-      diagram_name=$(awk '/@startuml/ { gsub("@startuml", "", $0); gsub(/^[ \t]+|[ \t]+$/, "", $0); print $0; exit }' "$pumlfile" | tr -cd 'A-Za-z0-9_-')
+
+      # Write .puml file INSIDE diagrams_dir, so all relative paths work
+      pumlfile_rel="diagrams/tmp_plantuml_extract_${block_num}.puml"
+      pumlfile_abs="$diagrams_dir/tmp_plantuml_extract_${block_num}.puml"
+      printf "%s\n" "${block_lines[@]}" > "$pumlfile_abs"
+      diagram_name=$(awk '/@startuml/ { gsub("@startuml", "", $0); gsub(/^[ \t]+|[ \t]+$/, "", $0); print $0; exit }' "$pumlfile_abs" | tr -cd 'A-Za-z0-9_-')
       if [[ -z "$diagram_name" ]]; then
         diagram_name="${mdname}-${block_num}"
       fi
       imgname="${diagram_name}.png"
       imgpath="diagrams/${imgname}"
 
-      # Render from out_mddir, writing to diagrams/
+      # Run PlantUML from the output .md's directory, input/output to diagrams/
       pushd "$out_mddir" >/dev/null
-      docker run --rm -v "$PWD":"$PWD" -w "$PWD" plantuml/plantuml "../../../../../$pumlfile" -tpng -o diagrams
+      docker run --rm -v "$PWD":"$PWD" -w "$PWD" plantuml/plantuml "$pumlfile_rel" -tpng
+      if [[ -f "$pumlfile_rel.png" ]]; then
+        mv "$pumlfile_rel.png" "$imgpath"
+      elif [[ -f "diagrams/tmp_plantuml_extract_${block_num}.png" ]]; then
+        mv "diagrams/tmp_plantuml_extract_${block_num}.png" "$imgpath"
+      fi
+      rm -f "$pumlfile_rel"
       popd >/dev/null
 
-      # Move and clean up
-      if [[ -f "$out_mddir/diagrams/tmp_plantuml_extract_${block_num}.png" ]]; then
-        mv "$out_mddir/diagrams/tmp_plantuml_extract_${block_num}.png" "$diagrams_dir/$imgname"
-      fi
-      rm -f "$pumlfile"
       updated_md+="![${diagram_name}](${imgpath})"$'\n'
     else
       updated_md+="$line"$'\n'
@@ -61,7 +65,4 @@ find "$INDIR" -type f -name "*.md" | while read -r mdfile; do
 
   mkdir -p "$out_mddir"
   printf "%s" "$updated_md" > "$out_mdfile"
-  # Clean up any tmp pngs in diagrams
-  rm -f "$out_mddir/diagrams/tmp_plantuml_extract_"*
-  rm -f tmp_plantuml_extract_*
 done
